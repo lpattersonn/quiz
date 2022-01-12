982a3bea45037ca78dad064fa11254ad67631a77
Leshan Patterson <leshan0627@gmail.com>
2022-01-12 02:31:11 +0000

    feat: update to create quiz post request function

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const morgan = require("morgan");
 const bodyParser = require('body-parser')
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // parse application/json
 app.use(bodyParser.json())
