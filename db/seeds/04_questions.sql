8fb53c52f0a04fa50b780e5e29c4637569915815
Leshan Patterson <leshan0627@gmail.com>
2022-01-22 15:55:02 +0000

    fix: Share quiz bug fixed by using an alert function to display the unique quiz URL

--- db/seeds/04_questions.sql
+++ db/seeds/04_questions.sql
@@ -14,3 +14,10 @@ True or false. The following code is fully functional and will work in a web bro
 INSERT INTO questions (id, quiz_id, question, answer) VALUES ( 3, 1, 'What is the use of a namespace in Web Development?','A namespace is a simple global object that is used to hold methods, properties, and other objects in them.');
 INSERT INTO questions (id, quiz_id, question, answer) VALUES ( 4, 1, 'What is SVG and why is it used?', 'SVG stands for Scalable Vector Graphics. It is used to display vector-based graphics over the web.');
 INSERT INTO questions (id, quiz_id, question, answer) VALUES ( 5, 1, 'What is the use of CORS?', 'CORS stands for Cross-origin Resource Sharing.');
+INSERT INTO questions (id, quiz_id, question, answer) VALUES ( 6, 2, 'How does CSS3 help in implementing rounded borders easily?', '
+The <border-radius> property that allows elements to be created with nice-looking rounded corners.');
+INSERT INTO questions (id, quiz_id, question, answer) VALUES ( 7, 2, '
+What is pagination? How can pagination be implemented?', 'A simple sequence of pages on a website. These pages are interconnected and have similar content to display to the users.');
+INSERT INTO questions (id, quiz_id, question, answer) VALUES ( 8, 2, 'What are the components of the CSS box model?','The CSS box model is used to represent an entity that encloses all of the HTML content into a box or a button element.');
+INSERT INTO questions (id, quiz_id, question, answer) VALUES ( 9, 2, 'Why are external style sheets preferred?', 'External style sheets provide an ample amount of advantages to developers.');
+INSERT INTO questions (id, quiz_id, question, answer) VALUES ( 10, 2, 'What is the meaning of long polling in Web Development?', 'A development pattern that is used to emulate a data push operation from a server to a client machine.');
